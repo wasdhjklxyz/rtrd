1d9f20604b92328c47255f4c7025bd3729ca51de
uiop <uiop@wasdhjkl.xyz>
2025-11-05 00:36:54 -0500

    refactor: remove header file

--- rtrd.c
+++ rtrd.c
@@ -1,9 +1,13 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-#include "rtrd.h"
-
 MODULE_LICENSE("GPL-2.0");
+
+#define RTRD_DBG(fmt, ...)                                             \
+	do {                                                           \
+		printk(KERN_DEBUG "[%s:%d] %s(): " fmt "\n", __FILE__, \
+		       __LINE__, __func__, ##__VA_ARGS__);             \
+	} while (0)
 
 static int rtrd_init(void)
 {
