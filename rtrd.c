c5dea2400684a1dbc8493d757f55651dda6648e8
uiop <uiop@wasdhjkl.xyz>
2025-11-04 23:14:10 -0500

    feat: add debug log macro

--- rtrd.c
+++ rtrd.c
@@ -1,17 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include "rtrd.h"
+
 MODULE_LICENSE("GPL-2.0");
 
 static int rtrd_init(void)
 {
-	printk(KERN_ALERT "hello world\n");
+	RTRD_DBG("hello world");
 	return 0;
 }
 
 static void rtrd_exit(void)
 {
-	printk(KERN_ALERT "goodbye\n");
+	RTRD_DBG("goodbye");
 }
 
 module_init(rtrd_init);
