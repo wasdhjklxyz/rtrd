155086419f93d64a795a7c984be56be07d82019f
uiop <uiop@wasdhjkl.xyz>
2025-11-11 04:21:57 -0500

    feat: loopback via NAPI
    - Introduce rx_queue for packets awaiting delivery to network stack
    - Implement rtrd_poll() to process queued packets
    - Clone outgoing packets in ndo_start_xmit and schedule NAPI for RX
    - Add carrier state management (netif_carrier_on/off)
    - Add skb_orphan() to break socket backpressure during loopback

--- rtrd.c
+++ rtrd.c
@@ -18,33 +18,78 @@ static struct net_device *rtrd_dev;
 struct rtrd_priv {
 	struct net_device_stats stats;
 	spinlock_t lock;
+	struct napi_struct napi;
+	struct sk_buff_head rx_queue;
 };
+
+static int rtrd_poll(struct napi_struct *napi, int budget)
+{
+	int work_done = 0;
+	struct rtrd_priv *priv = container_of(napi, struct rtrd_priv, napi);
+
+	while (work_done < budget) {
+		struct sk_buff *skb = skb_dequeue(&priv->rx_queue);
+		if (!skb) {
+			break;
+		}
+
+		skb->protocol = eth_type_trans(skb, napi->dev);
+		skb->ip_summed = CHECKSUM_UNNECESSARY;
+
+		priv->stats.rx_packets++;
+		priv->stats.rx_bytes += skb->len;
+
+		napi_gro_receive(napi, skb);
+		work_done++;
+	}
+
+	if (work_done < budget) {
+		napi_complete_done(napi, work_done);
+	}
+
+	return work_done;
+}
 
 static int rtrd_open(struct net_device *dev)
 {
+	struct rtrd_priv *priv = netdev_priv(dev);
 	static const u8 mac[ETH_ALEN] = { 0x00, 0x52, 0x54, 0x52, 0x44, 0x00 };
 
 	eth_hw_addr_set(dev, mac);
+	napi_enable(&priv->napi);
+
+	netif_carrier_on(dev);
 	netif_start_queue(dev);
+
 	return 0;
 }
 
 static int rtrd_stop(struct net_device *dev)
 {
+	struct rtrd_priv *priv = netdev_priv(dev);
+
+	napi_disable(&priv->napi);
+	netif_carrier_off(dev);
 	netif_stop_queue(dev);
 	return 0;
 }
 
 static netdev_tx_t rtrd_start_xmit(struct sk_buff *skb, struct net_device *dev)
 {
+	struct sk_buff *rx_skb;
 	struct rtrd_priv *priv = netdev_priv(dev);
 
 	RTRD_DBG("TX packet len=%u", skb->len);
 
 	priv->stats.tx_packets++;
 	priv->stats.tx_bytes += skb->len;
-	dev->stats.tx_packets++;
-	dev->stats.tx_bytes += skb->len;
+
+	rx_skb = skb_clone(skb, GFP_ATOMIC);
+	if (rx_skb) {
+		skb_orphan(rx_skb);
+		skb_queue_tail(&priv->rx_queue, rx_skb);
+		napi_schedule(&priv->napi);
+	}
 
 	dev_kfree_skb(skb);
 
@@ -99,11 +144,13 @@ static void rtrd_probe(struct net_device *dev)
 	priv = netdev_priv(dev);
 	memset(priv, 0, sizeof(struct rtrd_priv));
 	spin_lock_init(&priv->lock);
+	skb_queue_head_init(&priv->rx_queue);
 }
 
 static int __init rtrd_init(void)
 {
 	int ret;
+	struct rtrd_priv *priv;
 
 	rtrd_dev = alloc_netdev(sizeof(struct rtrd_priv), "rtrd%d",
 				NET_NAME_UNKNOWN, rtrd_probe);
@@ -111,6 +158,9 @@ static int __init rtrd_init(void)
 		RTRD_DBG("alloc_netdev failed");
 		return -ENOMEM; /* FIXME: This is just assumption */
 	}
+
+	priv = netdev_priv(rtrd_dev);
+	netif_napi_add(rtrd_dev, &priv->napi, rtrd_poll);
 
 	ret = register_netdev(rtrd_dev);
 	if (ret < 0) {
@@ -125,7 +175,10 @@ static int __init rtrd_init(void)
 
 static void __exit rtrd_exit(void)
 {
+	struct rtrd_priv *priv = netdev_priv(rtrd_dev);
+
 	if (rtrd_dev) {
+		netif_napi_del(&priv->napi);
 		unregister_netdev(rtrd_dev);
 		free_netdev(rtrd_dev);
 	}
