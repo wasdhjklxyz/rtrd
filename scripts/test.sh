f331130b81c081e973d19685bef3dbd9c6efad62
uiop <uiop@wasdhjkl.xyz>
2025-12-03 05:31:21 -0500

    test: set peer config

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,6 @@ rmmod $MODULE 2>/dev/null || true
 
 insmod /mnt/host/${MODULE}.ko
 ip link add $IFACE type $MODULE
+echo "192.0.2.$2" > "/sys/class/net/$IFACE/peer"
 ip addr add 10.0.0.$1/24 dev $IFACE
 ip link set $IFACE up
