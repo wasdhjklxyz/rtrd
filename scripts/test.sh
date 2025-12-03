8e9781d12e53ef7bb1ec2350f335e91a43f5dcb5
uiop <uiop@wasdhjkl.xyz>
2025-12-03 07:13:36 -0500

    fix: test script clarity

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,20 @@
 MODULE="rtrd"
 IFACE="rtrd0"
 
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <tunnel-ip> <peer-ip>"
+  exit 1
+fi
+
+TUNNEL_IP="$1"
+PEER_IP="$2"
+
 ip link set $IFACE down 2>/dev/null || true
+ip link del $IFACE down 2>/dev/null || true
 rmmod $MODULE 2>/dev/null || true
 
 insmod /mnt/host/${MODULE}.ko
 ip link add $IFACE type $MODULE
-echo "192.0.2.$2" > "/sys/class/net/$IFACE/peer"
-ip addr add 10.0.0.$1/24 dev $IFACE
+echo "$PEER_IP" > "/sys/class/net/$IFACE/peer"
+ip addr add $TUNNEL_IP/24 dev $IFACE
 ip link set $IFACE up
